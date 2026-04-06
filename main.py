0811dec16f2227c7a06aabe62f76cfaf3d1f4549
Massimo Pasquini <massimo.pasquini.dev@gmail.com>
2026-04-06 21:58:40 +0200

    Change EOD workflow: now runs its own script

--- main.py
+++
@@ -1,6 +0,0 @@
-from worker import *
-from clients import *
-
-
-
-update(CsvReader('cfg/eod/xmil/securities.csv'), wsclient_bit, 'www/eod/xmil')
---
+++ main_eod.py
@@ -0,0 +1,5 @@
+from worker import *
+from clients import *
+
+
+update( CsvReader('cfg/eod/xmil/securities.csv'), wsclient_bit, 'www/eod/xmil' )
